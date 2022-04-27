88d84d687779f33750a5e69e3c27216ec5e55b54
Rafael Krstačić <35665918+rkrstacic@users.noreply.github.com>
2022-04-27 18:17:27 +0200

    Reupload

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./assets/css/style.css";
-
-createApp(App).use(router).mount("#app");
--- src/views/Pricing.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="pricing">
-    <h1>This is a pricing page</h1>
-  </div>
-</template>
