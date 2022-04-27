47d9b5fbdb92c25d89050c9aac0c40e205810780
Rafael Krstačić <35665918+rkrstacic@users.noreply.github.com>
2022-04-27 18:50:58 +0200

    Tailwind/Vuetify

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,27 @@ import firebase from "firebase/app";
 import { initializeApp } from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+} from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyChdPYY4VC5t91L5VO8t171_Nliv6WvqU8",
-	authDomain: "shop-locator-fipu.firebaseapp.com",
-	projectId: "shop-locator-fipu",
-	storageBucket: "shop-locator-fipu.appspot.com",
-	messagingSenderId: "61191661633",
-	appId: "1:61191661633:web:52a4f9336ae988580d7ec0",
+    apiKey: "AIzaSyChdPYY4VC5t91L5VO8t171_Nliv6WvqU8",
+    authDomain: "shop-locator-fipu.firebaseapp.com",
+    projectId: "shop-locator-fipu",
+    storageBucket: "shop-locator-fipu.appspot.com",
+    messagingSenderId: "61191661633",
+    appId: "1:61191661633:web:52a4f9336ae988580d7ec0",
 };
 
 // Initialization
 const app = initializeApp(firebaseConfig);
 
-export { firebase, createUserWithEmailAndPassword, getAuth };
+export {
+    firebase,
+    createUserWithEmailAndPassword,
+    getAuth,
+    onAuthStateChanged,
+};
