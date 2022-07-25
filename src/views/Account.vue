5f65b2e7ae7ef512f843709974d98c7dd2d03147
Rafael Krstačić <35665918+rkrstacic@users.noreply.github.com>
2022-07-25 18:41:37 +0200

    Added account view

---
+++ src/views/Account.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="account">
+		<v-container>
+			<v-card class="text-left">
+				<v-row>
+					<v-col cols="3">Avatar</v-col>
+					<v-col cols="9">
+						<v-row>
+							<v-col cols="9">
+								<v-row><v-col>Email</v-col></v-row>
+								<v-row><v-col>Something</v-col></v-row>
+							</v-col>
+							<v-col cols="3">
+								<v-row><v-col>Edit</v-col></v-row>
+								<v-row><v-col>Edit</v-col></v-row>
+							</v-col>
+						</v-row>
+					</v-col>
+				</v-row>
+			</v-card>
+		</v-container>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "account",
+	created() {
+		alert("In progress");
+	},
+};
+</script>
