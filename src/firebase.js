fcc1f7cd2894e79c3ad70105cfeaf801cc4d4f5b
Rafael Krstačić <35665918+rkrstacic@users.noreply.github.com>
2022-08-24 12:07:58 +0200

    Renewed Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -25,6 +25,8 @@ import {
 } from "firebase/auth";
 import { Firestore } from "firebase/firestore";
 
+// Old Project
+/*
 const firebaseConfig = {
 	apiKey: "AIzaSyChdPYY4VC5t91L5VO8t171_Nliv6WvqU8",
 	authDomain: "shop-locator-fipu.firebaseapp.com",
@@ -32,6 +34,15 @@ const firebaseConfig = {
 	storageBucket: "shop-locator-fipu.appspot.com",
 	messagingSenderId: "61191661633",
 	appId: "1:61191661633:web:52a4f9336ae988580d7ec0",
+};*/
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBu1U3wRyA9fmiOTkKeXRFvwKnHqOz4Rig",
+	authDomain: "shop-locator-fipu-a9f7d.firebaseapp.com",
+	projectId: "shop-locator-fipu-a9f7d",
+	storageBucket: "shop-locator-fipu-a9f7d.appspot.com",
+	messagingSenderId: "1042101196765",
+	appId: "1:1042101196765:web:4d9ac1a88476f7404fc834",
 };
 
 // Initialization
